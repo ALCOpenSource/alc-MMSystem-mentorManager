6873d3ab122c9955420fba3e66fed9b327d5fd05
Nada Feteiha <nada.feteiha@gmail.com>
2023-01-29 23:43:38 -0800

    add search

---
+++ app/src/main/java/com/peculiaruc/alc_mmsystem_mentormanager/ui/search/SearchAdapter.kt
@@ -0,0 +1,178 @@
+package com.peculiaruc.alc_mmsystem_mentormanager.ui.search
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.peculiaruc.alc_mmsystem_mentormanager.data.models.*
+import com.peculiaruc.alc_mmsystem_mentormanager.databinding.ItemSearchCertificateBinding
+import com.peculiaruc.alc_mmsystem_mentormanager.databinding.ItemSearchProgramBinding
+import com.peculiaruc.alc_mmsystem_mentormanager.databinding.ItemSearchReportBinding
+import com.peculiaruc.alc_mmsystem_mentormanager.databinding.ItemSearchTaskBinding
+
+/**
+ * enum class for the items to be displayed in the search fragment
+ */
+enum class ViewType(val value: Int) {
+    Task(1), Program(2), Certificate(3), Report(4)
+}
+
+/**
+ * recycler view adapter for the search fragment
+ */
+class SearchAdapter(
+    private val itemsList: List<Item>
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        when (viewType) {
+            ViewType.Task.value -> {
+                val binding = ItemSearchTaskBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false
+                )
+                return TaskViewHolder(binding)
+            }
+            ViewType.Program.value -> {
+                val binding = ItemSearchProgramBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false
+                )
+                return ProgramViewHolder(binding)
+            }
+            ViewType.Certificate.value -> {
+                val binding = ItemSearchCertificateBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false
+                )
+                return CertificateViewHolder(binding)
+            }
+            ViewType.Report.value -> {
+                val binding = ItemSearchReportBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false
+                )
+                return ReportViewHolder(binding)
+            }
+            else -> {
+                val binding = ItemSearchTaskBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false
+                )
+                return TaskViewHolder(binding)
+            }
+        }
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (itemsList[position]) {
+            is Task -> {
+                (holder as TaskViewHolder).bind(itemsList[position] as Task)
+            }
+            is Program -> {
+                (holder as ProgramViewHolder).bind(itemsList[position] as Program)
+            }
+            is Certificate -> {
+                (holder as CertificateViewHolder).bind(itemsList[position] as Certificate)
+            }
+            is Report -> {
+                (holder as ReportViewHolder).bind(itemsList[position] as Report)
+            }
+        }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return when (itemsList[position]) {
+            is Task -> ViewType.Task.value
+            is Program -> ViewType.Program.value
+            is Certificate -> ViewType.Certificate.value
+            else -> ViewType.Report.value
+        }
+    }
+
+    /**
+     *  handles the changes in the recycler view
+     */
+    companion object ItemDiffCallback : DiffUtil.ItemCallback<Item>() {
+        override fun areItemsTheSame(oldItem: Item, newItem: Item): Boolean {
+            return oldItem == newItem
+        }
+
+        override fun areContentsTheSame(oldItem: Item, newItem: Item): Boolean {
+//            return oldItem.id == newItem.id
+            return true
+        }
+    }
+
+    /**
+     * View holder for the task item
+     */
+    class TaskViewHolder(private val binding: ItemSearchTaskBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        /**
+         * binds the task item to the view holder
+         */
+        fun bind(item: Item) {
+            binding.apply {
+                textTitle.text = (item as Task).title
+                textDate.text = item.time
+            }
+        }
+    }
+
+    /**
+     * View holder for the program item
+     */
+    class ProgramViewHolder(private val binding: ItemSearchProgramBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        /**
+         * binds the program item to the view holder
+         */
+        fun bind(item: Item) {
+            binding.textTitle.text = (item as Program).programTitle
+        }
+    }
+
+    /**
+     * View holder for the certificate item
+     */
+    class ReportViewHolder(private val binding: ItemSearchReportBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        /**
+         * binds the report item to the view holder
+         */
+        fun bind(item: Item) {
+            binding.apply {
+                textTitle.text = (item as Report).title
+                textOwner.text = item.author
+                textDate.text = item.date
+            }
+        }
+    }
+
+    /**
+     * View holder for the report item
+     */
+    class CertificateViewHolder(private val binding: ItemSearchCertificateBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        /**
+         * binds the certificate item to the view holder
+         */
+        fun bind(item: Item) {
+            binding.apply {
+                textTitle.text = (item as Certificate).title
+            }
+        }
+    }
+
+    override fun getItemCount(): Int = itemsList.size
+
+}
---
+++ app/src/main/java/com/peculiaruc/alc_mmsystem_mentormanager/ui/search/SearchFragment.kt
@@ -0,0 +1,83 @@
+package com.peculiaruc.alc_mmsystem_mentormanager.ui.search
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
+import com.peculiaruc.alc_mmsystem_mentormanager.R
+import com.peculiaruc.alc_mmsystem_mentormanager.data.models.Certificate
+import com.peculiaruc.alc_mmsystem_mentormanager.data.models.Program
+import com.peculiaruc.alc_mmsystem_mentormanager.data.models.Report
+import com.peculiaruc.alc_mmsystem_mentormanager.data.models.Task
+import com.peculiaruc.alc_mmsystem_mentormanager.databinding.FragmentSearchBinding
+import com.peculiaruc.alc_mmsystem_mentormanager.utilities.type.ProgramProgress
+import com.peculiaruc.alc_mmsystem_mentormanager.utilities.type.TaskStatus
+
+/**
+ * this represent Search screen..
+ */
+class SearchFragment : Fragment() {
+    private lateinit var binding: FragmentSearchBinding
+
+    private val navController by lazy {
+        NavHostFragment.findNavController(this)
+    }
+
+    private val list = listOf(
+        Task("Retrofit library article write", "3 days from now", TaskStatus.ASSIGN),
+        Program("Google Africa Scholarship Program", "", ProgramProgress.ADD),
+        Certificate("", "GADS CLOUD 2022 CERTIFICATE"),
+        Task("Room library article write", "3 days from now", TaskStatus.ASSIGN),
+        Report(
+            "Google Africa Scholarship Report", "By Marawan Kabir ",
+            "19th - 25th Oct 22"
+        ),
+        Certificate("", "GADS ANDROID 2022 CERTIFICATE"),
+        Task("Dagger library article write", "5 days from now", TaskStatus.COMPLETED),
+        Program("Google America Scholarship Program", "", ProgramProgress.CHECK),
+        Task("hilt library article write", "4 days from now", TaskStatus.ASSIGN),
+        Program("Google Asia Scholarship Program", "", ProgramProgress.DOUBLE_CHECK),
+        Certificate("", "GADS Web 2022 CERTIFICATE"),
+        Report(
+            "Google Asia Scholarship Report", "By Ibrahim Kabir ",
+            "14th - 26th Oct 22"
+        ),
+        Certificate("", "GADS React 2022 CERTIFICATE"),
+        Certificate("", "GADS Angular 2022 CERTIFICATE"),
+        Task("Angular library article write", "2 days from now", TaskStatus.ASSIGNED),
+        Report(
+            "Google America Scholarship Report", "By Muhammad Kabir ",
+            "19th - 25th Oct 22"
+        ),
+        Program("Google Scholarship Program", "", ProgramProgress.CHECK),
+        Report(
+            "Google Scholarship Report", "By Nada Kabir ",
+            "11th - 15th Oct 22"
+        ),
+        Certificate("", "GADS TF 2022 CERTIFICATE"),
+    )
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_search, container, false)
+
+        binding.recyclerViewSearch.adapter = SearchAdapter(list)
+        binding.appBarLayout.elevation = 0f
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // handle back button
+        binding.topAppBar.setNavigationOnClickListener {
+            navController.navigateUp()
+        }
+    }
+}
